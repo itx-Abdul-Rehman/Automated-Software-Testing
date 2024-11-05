e6c4f2044a384f8f6521211c752c39ff6b1aafc5
Mohammad Abdul Rehman <itsrehman2004@gmail.com>
2024-11-05 21:37:04 +0500

    .

--- Signup-Page-Script/src/Main.java
+++ Signup-Page-Script/src/Main.java
@@ -1,9 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.nio.file.WatchEvent;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://localhost:5173/login");
+
+        
 
     }
 }
