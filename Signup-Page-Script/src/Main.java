5d7ba3e79987c6a4e87d70c399fd30d22f02159d
Mohammad Abdul Rehman <itsrehman2004@gmail.com>
2024-11-07 22:35:42 +0500

    .

--- Signup-Page-Script/src/Main.java
+++ Signup-Page-Script/src/Main.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.nio.file.WatchEvent;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,6 +14,7 @@ public class Main {
         WebDriver driver=new ChromeDriver();
         driver.get("http://localhost:5173/");
 
+        String currentUrl=driver.getCurrentUrl();
         WebElement usernameFiled=driver.findElement(By.name("username"));
         WebElement emailField=driver.findElement(By.name("email"));
         WebElement passwordField= driver.findElement(By.name("password"));
@@ -23,9 +25,20 @@ public class Main {
         passwordField.sendKeys("m.abdul786@");
         confirmPasswordField.sendKeys("m.abdul786@");
 
-        WebElement registerButton= driver.findElement(By.cssSelector());
+        WebElement registerButton= driver.findElement(By.cssSelector("input[value=Register]"));
+        System.out.println(registerButton.getAttribute("value"));
 
+        registerButton.click();
+        String newUrl=driver.getCurrentUrl();
+        System.out.println(currentUrl);
+        System.out.println(newUrl);
+        if(!currentUrl.equals(newUrl)){
+            System.out.println("Test Case Passed");
+        }else{
+            System.out.println("Test Case Failed");
+        }
 
+        driver.quit();
 
     }
 }
