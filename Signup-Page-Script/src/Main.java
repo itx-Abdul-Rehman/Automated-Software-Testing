36ba0f5ca909aec94cf699700e59ea2811310f9a
Mohammad Abdul Rehman <itsrehman2004@gmail.com>
2024-11-07 22:02:15 +0500

    .

--- Signup-Page-Script/src/Main.java
+++ Signup-Page-Script/src/Main.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.nio.file.WatchEvent;
@@ -9,9 +11,21 @@ public class Main {
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 
         WebDriver driver=new ChromeDriver();
-        driver.get("http://localhost:5173/login");
+        driver.get("http://localhost:5173/");
 
-        
+        WebElement usernameFiled=driver.findElement(By.name("username"));
+        WebElement emailField=driver.findElement(By.name("email"));
+        WebElement passwordField= driver.findElement(By.name("password"));
+        WebElement confirmPasswordField= driver.findElement(By.name("confirmPassword"));
+
+        usernameFiled.sendKeys("Mohammad Abdul-Rehman");
+        emailField.sendKeys("m.abdul@gmail.com");
+        passwordField.sendKeys("m.abdul786@");
+        confirmPasswordField.sendKeys("m.abdul786@");
+
+        WebElement registerButton= driver.findElement(By.cssSelector());
+
+
 
     }
 }
